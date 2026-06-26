9d714feae31ddd2891b6fa1dcba29b71ecb9146e
barathwaj002 <s.barathwaj002@gmail.com>
2026-06-27 00:30:40 +0530

    Implement Delete Product API

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -159,9 +159,40 @@ const updateProduct = async (req, res) => {
     }
 };
 
+// Delete Product
+const deleteProduct = async (req, res) => {
+    try {
+
+        const product = await Product.findById(req.params.id);
+
+        if (!product) {
+            return res.status(404).json({
+                success: false,
+                message: "Product not found"
+            });
+        }
+
+        await Product.findByIdAndDelete(req.params.id);
+
+        res.status(200).json({
+            success: true,
+            message: "Product deleted successfully"
+        });
+
+    } catch (error) {
+
+        res.status(500).json({
+            success: false,
+            message: error.message
+        });
+
+    }
+};
+
 module.exports = {
     addProduct,
     getProducts,
     getProductById,
-    updateProduct
+    updateProduct,
+    deleteProduct
 };
--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const router = express.Router();
 
-const { addProduct,getProducts,getProductById,updateProduct } = require("../controllers/productController");
+const { addProduct,getProducts,getProductById,updateProduct,deleteProduct } = require("../controllers/productController");
 
 // Add Product
 router.post("/", addProduct);
 router.get("/", getProducts);   
 router.get("/:id", getProductById);
 router.put("/:id", updateProduct);
+router.delete("/:id", deleteProduct);
 
 module.exports = router;
