387a9b08ea05430e99d0d5ff57effe1593b29b0f
barathwaj002 <s.barathwaj002@gmail.com>
2026-06-27 09:24:33 +0530

    Implement Role Based Access Control (RBAC)

--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const router = express.Router();
+const protect = require("../middleware/authMiddleware");
+const admin = require("../middleware/adminMiddleware");
 
 const { addProduct,getProducts,getProductById,updateProduct,deleteProduct } = require("../controllers/productController");
 
 // Add Product
-router.post("/", addProduct);
+router.post("/", protect, admin, addProduct);
 router.get("/", getProducts);   
 router.get("/:id", getProductById);
-router.put("/:id", updateProduct);
-router.delete("/:id", deleteProduct);
+router.put("/:id", protect, admin, updateProduct);
+router.delete("/:id", protect, admin, deleteProduct);
 
 module.exports = router;
