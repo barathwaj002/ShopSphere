743986e5ce791189123c06f9f379776cc2978ff7
barathwaj002 <s.barathwaj002@gmail.com>
2026-06-27 19:27:29 +0530

    Create Order model

---
+++ server/models/Order.js
@@ -0,0 +1,59 @@
+const mongoose = require("mongoose");
+
+const orderSchema = new mongoose.Schema(
+    {
+        user: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true
+        },
+
+        orderItems: [
+            {
+                product: {
+                    type: mongoose.Schema.Types.ObjectId,
+                    ref: "Product",
+                    required: true
+                },
+
+                quantity: {
+                    type: Number,
+                    required: true
+                }
+            }
+        ],
+
+        shippingAddress: {
+            type: String,
+            required: true
+        },
+
+        totalPrice: {
+            type: Number,
+            required: true
+        },
+
+        paymentStatus: {
+            type: String,
+            enum: ["Pending", "Paid"],
+            default: "Pending"
+        },
+
+        orderStatus: {
+            type: String,
+            enum: [
+                "Processing",
+                "Shipped",
+                "Delivered",
+                "Cancelled"
+            ],
+            default: "Processing"
+        }
+
+    },
+    {
+        timestamps: true
+    }
+);
+
+module.exports = mongoose.model("Order", orderSchema);
